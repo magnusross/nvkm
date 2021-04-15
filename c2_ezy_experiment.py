50a325f92114ba5a1eb6bc580447ea4f65f11968
Magnus <magnusaross@gmail.com>
2021-04-15 17:26:32 +0100

    new experiments

--- c2_ezy_experiment.py
+++ c2_ezy_experiment.py
@@ -36,34 +36,42 @@ Nvg = args.Nvg
 
 tf = jnp.linspace(-3, 3, 100)
 t1 = jnp.linspace(-3, 3, Nvg).reshape(-1, 1)
-t2 = 6. * jrnd.uniform(keys[0], shape=(Nvg, 2)) - 3.
+t2 = 6.0 * jrnd.uniform(keys[0], shape=(Nvg, 2)) - 3.0
 
 model2 = NVKM(
     zgs=[t1, t2],
-    vgs = [jrnd.normal(keys[3], shape=(Nvg,)), jrnd.normal(keys[2], shape=(Nvg,))],
+    vgs=[None, None],
     zu=jnp.linspace(-15, 15, Nvu).reshape(-1, 1),
     vu=jrnd.normal(keys[1], shape=(Nvu,)),
     C=2,
     lsgs=args.lsgs,
     ampgs=args.ampsgs_init,
-    )
-    
-plot_c2_filter_multi(model2, tf, 15, save=args.f_name + 'non_var_c2f.png', variational=False)
+    alpha=args.alpha,
+)
+model2.vgs = [
+    model2.g_gps[0].sample(model2.zgs[0], 1).flatten(),
+    model2.g_gps[1].sample(model2.zgs[1], 1, key=jrnd.PRNGKey(1010101)).flatten(),
+]
+model2.vu = model2.u_gp.sample(model2.zu, 1).flatten()
+model2.g_gps = model2.set_G_gps(args.ampsgs_init, args.lsgs)
+model2.u_gp = model2.set_u_gp(args.ampu, args.lsu)
+# plot_c2_filter_multi(model2, tf, 15, save=args.f_name + 'non_var_c2f.png', variational=False)
 # plt.show()
 
 x = jnp.linspace(-15, 15, args.Ndata)
-y = model2.sample(x, N_s=1, key=keys[6]).flatten() + noise*jrnd.normal(keys[9], shape=(args.Ndata,))
+y = model2.sample(x, N_s=1, key=keys[6]).flatten() + noise * jrnd.normal(
+    keys[9], shape=(args.Ndata,)
+)
 data = (x, y)
 
-model2.plot_samples(x, 5, save=args.f_name + 'non_var_samps.png')
+# model2.plot_samples(x, 5, save=args.f_name + 'non_var_samps.png')
 
 q_pars_init = {
-    "LC_gs": [args.q_frac*gp.LKvv for gp in model2.g_gps],
+    "LC_gs": [args.q_frac * gp.LKvv for gp in model2.g_gps],
     "mu_gs": model2.vgs,
-    "LC_u": args.q_frac*model2.u_gp.LKvv,
+    "LC_u": args.q_frac * model2.u_gp.LKvv,
     "mu_u": model2.vu,
 }
-
 
 
 var_model2 = VariationalNVKM(
@@ -84,16 +92,36 @@ dont_fit = []
 if not bool(args.fit_noise):
     dont_fit.append("noise")
 
-plot_c2_filter_multi(var_model2, tf, 10, save=args.f_name + 'var_c2f.png', variational=True)
-var_model2.plot_samples(jnp.linspace(-15, 15, 250), 15, save=args.f_name + "c2_samps_pre.png")
-var_model2.plot_filters(tf, 10, save=args.f_name + "c2_filter_pre.png")
-
-
-var_model2.fit(args.Nits, args.lr, args.Nbatch, args.Ns, dont_fit=dont_fit)
-
+# plot_c2_filter_multi(var_model2, tf, 10, save=args.f_name + 'var_c2f.png', variational=True)
 var_model2.plot_samples(
-    jnp.linspace(-15, 15, 250), 15, save=args.f_name + "fit_samps.png"
+    jnp.linspace(-15, 15, 100), 5, save=args.f_name + "pre_samps.png"
 )
-var_model2.plot_filters(
-    tf, 10, save=args.f_name + "fit_filter.png"
+var_model2.plot_filters(tf, 5, save=args.f_name + "pre_filter_pre.png")
+
+
+failed_pars = var_model2.fit(
+    args.Nits, args.lr, args.Nbatch, args.Ns, dont_fit=dont_fit
 )
+if failed_pars:
+    # f_model = VariationalNVKM(
+    #     [t1, t2],
+    #     jnp.linspace(-15, 15, Nvu).reshape(-1, 1),
+    #     data,
+    #     IndependentGaussians,
+    #     q_pars_init=failed_pars["q_pars"],
+    #     lsgs=args.lsgs,
+    #     ampgs_init=failed_pars["ampgs"],
+    #     noise_init=noise,
+    #     alpha=args.alpha,
+    #     lsu=args.lsu,
+    #     ampu=args.ampu,
+    #     C=2,
+    # )
+
+    # f_model.plot_filters(tf, 10, save=args.f_name + "failed_filters.png")
+    print(failed_pars)
+else:
+    var_model2.plot_samples(
+        jnp.linspace(-15, 15, 150), 5, save=args.f_name + "fit_samps.png"
+    )
+    var_model2.plot_filters(tf, 10, save=args.f_name + "fit_filter.png")
